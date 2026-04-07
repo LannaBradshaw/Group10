326ed0ba857b8d5f70f05cce114b84c3fa1b53f7
Van Montgomery <vazenroy@bee.cs.colostate.edu>
2026-04-07 14:57:28 -0600

    Started creation of homepage

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
+<!DOCTYPE html>
 
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <header>
+            <img src="logo.png" alt="Company Logo" style="width:200px;height:200px">
+            <h1>HueMaxer</h1>
+        </header>
+
+    </body>
+</html>

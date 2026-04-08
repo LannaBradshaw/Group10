acd233d72d8a5c85bd792c79be32fd29d1e9c3d5
Van Montgomery <vazenroy@bee.cs.colostate.edu>
2026-04-08 11:43:19 -0600

    Added form input for row and col

--- color.php
+++ color.php
@@ -1,1 +1,28 @@
+<!DOCTYPE html>
 
+<html>
+    <head>
+        <title>Color Coordinate - HueMaxer</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        <header>
+            <h1>Color Coordinate</h1>
+        </header>
+
+        <nav>
+            <a href="index.php">Homepage</a>
+            <a href="about.php">About</a>
+            <a href="color.php">Color Coordinate</a>
+        </nav>
+
+        <div class="page_body">
+            <form action="<?php $_PHP_SELF ?>" method="POST">
+                Row (1 - 26): <input type = "text" name = "row" />
+                Column (1 - 10): <input type = "text" name = "col" />
+                <input type = "submit" />
+            </form>
+        </div>
+    </body>
+</html>

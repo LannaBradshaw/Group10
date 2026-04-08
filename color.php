c90fece850ad74cfba14160061677aa47c9dbbaf
Van Montgomery <vazenroy@bee.cs.colostate.edu>
2026-04-08 12:34:39 -0600

    Added primitive functionality and checking for grid size and color cnt

--- color.php
+++ color.php
@@ -1,5 +1,33 @@
 <!DOCTYPE html>
 
+<?php
+    $grid_size = -1;
+    $colors = -1;
+    $message = "";
+
+    if(isset($_POST['grid_size']) && isset($_POST['colors'])){
+        if(is_numeric($_POST['grid_size']) && is_numeric($_POST['colors'])){
+            $grid_size = intval($_POST['grid_size']);
+            $colors = intval($_POST['colors']);
+
+            if($grid_size < 1 || $grid_size > 26){
+                $message = "Grid size should be between 1 - 26";
+            }
+            elseif($colors < 1 || $colors > 10){
+                $message = "Color count should be between 1 - 10";
+            }
+            else
+                $message = "Grid Size: ". $grid_size. "<br /> Color Count: ". $colors;
+        }
+        elseif(is_numeric($_POST['grid_size']) || is_numeric($_POST['colors'])){
+            $message = "Please enter both a grid size and color count";
+        }
+        elseif($_POST["grid_size"] || $_POST["colors"]){
+            $message = "Grid size and color count must be integers";
+        }
+    }
+?>
+
 <html>
     <head>
         <title>Color Coordinate - HueMaxer</title>
@@ -18,11 +46,12 @@
         </nav>
 
         <div class="page_body">
-            <form action="<?php $_PHP_SELF ?>" method="POST">
-                Row (1 - 26): <input type = "text" name = "row" />
-                Column (1 - 10): <input type = "text" name = "col" />
+            <form method="POST">
+                Grid Size (1 - 26): <input type = "text" name = "grid_size" />
+                Number of Colors (1 - 10): <input type = "text" name = "colors" />
                 <input type = "submit" />
             </form>
+            <p><?php echo $message; ?></p>
         </div>
     </body>
 </html>

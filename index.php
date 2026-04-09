653189060d9ddefdf4cb97a35830feb3026cd90a
Van Montgomery <vazenroy@bee.cs.colostate.edu>
2026-04-09 17:00:25 -0600

    Updated homepage and css

--- index.php
+++ index.php
@@ -19,7 +19,10 @@
         </nav>
 
         <div class="page_body">
-            <p>Test text for styling purposes</p>
+            <h2>Welcome to HueMaxer!</h2>
+            <h3>We are a small team of Computer Science students at CSU working to create a functional website as part of our web design course! </br> We're so glad you're here!</h3>
+            <p>Check out our About page to learn more about us!</p>
+            <p>For color coordinates, check out our Color Coordinate page!</p>
         </div>
     </body>
 </html>
